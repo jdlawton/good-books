08c24dda0e097155440669de25250f880d0eca37
JoeL <jlawton1203@gmail.com>
2020-10-16 15:36:25 -0500

    Added server/schemas and typeDefs for User, Book, Auth, Query(me), Mutation(login, addUser, saveBook, removeBook). Added resolvers for Query(me), and Mutation(login, addUser). Implementing this included installing and setting up the Apollo server to implement GraphQL and refactoring the server/utils/auth.js file to make authMiddleware work with GraphQL api.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 const routes = require('./routes');
+const {ApolloServer} = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth');
+
+//import typeDefs and resolvers
+const {typeDefs, resolvers} = require('./schemas');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context : authMiddleware
+});
+
+//integrate Apollo server with the Express application as middleware
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +33,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  //log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
