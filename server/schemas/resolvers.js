06aaf322097fbf04352a69218fe19d3162433a68
JoeL <jlawton1203@gmail.com>
2020-10-16 20:17:58 -0500

    Added resolvers for saveBook and removeBook and tested them in GraphQL Playground

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,10 +7,10 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user){
+                //console.log(context.user._id);
                 const userData = await User.findOne({_id: context.user._id})
-                    .select('-__v -password')
-                    .populate('savedBooks');
-                
+                    .select('-__v -password');
+                //console.log(userData);
                 return userData;
             }
             throw new AuthenticationError('Not logged in');
@@ -39,8 +39,33 @@ const resolvers = {
 
             const token = signToken(user);
             return {token, user};
-        }
-    }
+        },
+        // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: {authors: args.authors, description: args.description, title: args.title, bookId: args.bookId, image: args.image, link: args.link}}},
+                    {new: true}
+                );
+                console.log(updatedUser);
+                return updatedUser;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId: args.bookId}}},
+                    {new: true}
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+    },
 
 };
 
